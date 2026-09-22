909da751c31a0a19b5e78baa5ba20c6dd7dcb096
Marlou Dev <web@empireonegroup.com>
2026-09-23 00:31:01 +0800

    added new adjustment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,9 +6,9 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
@@ -17,7 +17,9 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
-        //
+        $middleware->validateCsrfTokens(except: [
+            'api/save_case_file_email_response',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
